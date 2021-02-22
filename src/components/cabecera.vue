6ef2ce2dd9d9a951acedc8827e4249f14e36a7db
juandemarr <cloud15jc@gmail.com>
2021-02-22 01:07:41 +0100

    login almost done

--- src/components/cabecera.vue
+++ src/components/cabecera.vue
@@ -4,7 +4,7 @@
 
     <nav class="navbar navbar-expand-lg navbar-light">
       <div class="navbar-brand">
-        <a href="#/"><img class="logo mr-2" src="../../public/phoenix2.svg" alt="Logo PhoneixGames">PhoenixGames</a>
+        <router-link to="/"><img class="logo mr-2" src="../../public/phoenix2.svg" alt="Logo PhoneixGames">PhoenixGames</router-link>
       </div>
       <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
@@ -12,13 +12,21 @@
       <div class="collapse navbar-collapse" id="navbarNav">
         <ul class="navbar-nav ml-auto">
           <li class="nav-item">
-            <a class="nav-link" href="#/login">Login</a>
+            <!-- <a class="nav-link" href="#/login">Iniciar Sesion</a> -->
+            <div >
+              
+            </div>
+            <a v-if="authenticated">{{user.data.email}}</a>
+            <router-link to="/login" v-if="!authenticated">Iniciar sesion</router-link>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="#">Register</a>
+            <!-- <a class="nav-link" href="#">Registrarse</a> -->
+            <a @click="logout" v-if="authenticated">Cerrar sesion</a>
+            <router-link to="/#" v-if="!authenticated">Registrarse</router-link>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="#/carrito">Shopping Cart</a>
+            <!-- <a class="nav-link" href="#/carrito">Carrito de compra</a> -->
+            <router-link to="/carrito" v-if="authenticated">Carrito de compra</router-link>
           </li>
         </ul>
       </div>
@@ -30,24 +38,53 @@
 
 <script lang="js">
 /* import carrito from './components/carrito.vue' */
-
+import firebase from '../db.js'
   export default  {
     name: 'cabecera',
     props: [],
-    mounted () {
+    mounted(){
+      firebase.auth.onAuthStateChanged( user => {
+        if (user) {
+          this.user.loggedIn = true;
+          this.user.data = user;
+        }
+        else {
+          this.user.loggedIn = false;
+          this.user.data = {};
+        }
+      })
+
 
     },
     data () {
       return {
-
+        user: {
+          loggedIn: false,
+          data: {}
+        },
+        //nombreUsuario:""
       }
     },
     methods: {
-
+      logout:function (){
+        firebase.logout()
+        
+      },
+      /* recibirUsuario:function(user){
+        this.nombreUsuario=user.data.email;
+      } */
     },
     computed: {
-
-    },
+          authenticated(){
+              return this.user.loggedIn
+          },
+          /* firstName(){
+            if (this.user.data.displayName) {
+              return this.user.data.displayName.split(' ')[0]
+            }
+            return null
+          } */
+      },
     components:{
           /* carrito */
     }
--- src/components/login.vue
+++ src/components/login.vue
@@ -5,40 +5,89 @@
     <form>
       <div>
         <label for="email">Correo electrónico: </label>
-        <input type="email" id="email">
+        <input type="email" id="email" v-model="email">
       </div>
       <div>
         <label for="password">Contraseña: </label>
-        <input type="password" id="password">
+        <input type="password" id="password" v-model="password">
       </div>
+      <button class="btn" @click='iniciarCorreo(email,password)'>Iniciar sesion</button>
+      <button class="btn" @click='registro(email,password)'>Registrarse</button>
     </form>
 
-    <p>Inicar sesión con:
-      
-    </p>
+    <p>Inicar sesión con:</p>
+    <button class="logginGoogle" @click="login"><i class="fab fa-google"></i></button>
   </section>
 
 </template>
 
 <script lang="js">
+/* import {db}  from '../db.js'; */
+/*para el logeo*/
+import firebase from '../db.js'
 
   export default  {
     name: 'login',
     props: [],
-    mounted () {
-
-    },
+    
     data () {
       return {
+        user: {
+          loggedIn: false,
+          data: {}
+        },
+        email:"",
+        password:"",
+      }
+      
+    },
 
+    mounted(){
+      firebase.auth.onAuthStateChanged( user => {
+        if (user) {
+          this.user.loggedIn = true;
+          this.user.data = user;
+        }
+        else {
+          this.user.loggedIn = false;
+          this.user.data = {};
+        }
+      })
+
+    },
+
+    
+    methods: {
+      login:function(){
+        firebase.login();
+        /* if(this.authenticated)
+          this.$router.push('/'); */
+      },
+
+      registro:function(email,password){
+        //console.log("antes de enviar: "+email +" " +password);
+        firebase.registrarseBD(email,password);
+          if(this.authenticated){
+            this.$router.push('/');
+          }
+      },
+
+      iniciarCorreo:function(email,password){
+        firebase.loginCorreo(email,password);
       }
     },
-    methods: {
-
-    },
     computed: {
-
+      authenticated(){
+          return this.user.loggedIn
+      }
+      /* firstName(){
+        if (this.user.data.displayName) {
+          return this.user.data.displayName.split(' ')[0]
+        }
+        return null
+      } */
     }
+    
 }
 
 
